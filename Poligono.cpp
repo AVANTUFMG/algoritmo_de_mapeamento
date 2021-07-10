55c86958a048397292ea4cba0f820a5fd5634134
arthurnader <82470629+arthurnader@users.noreply.github.com>
2021-07-10 14:04:03 -0300

    Update Poligono.cpp

--- Poligono.cpp
+++ Poligono.cpp
@@ -41,3 +41,149 @@ void Poligono::mostrarTodasArestas()
 		iterator.mostrarAresta();
 	}
 }
+
+bool Poligono::ehConvexo()
+{
+	vector<Point> vertices2 = vertices;
+	vertices2.push_back(vertices[0]);
+	vertices2.push_back(vertices[1]);
+
+	for (int i = 0; i < vertices.size(); i++) {
+		Aresta diagonalAnalisada(vertices2[i], vertices2[i + 2]);
+		double projecao = vertices2[i + 1].get_x() * diagonalAnalisada.getCoefAngular() + diagonalAnalisada.getCoefLinear();
+
+		if (diagonalAnalisada.getCoefAngular() == DBL_MAX) {
+
+			if (vertices2[i].get_y() < vertices2[i + 2].get_y()) {
+				if (vertices2[i + 1].get_x() < vertices2[i].get_x()) {
+					return false;
+				}
+			}
+			else {
+				if (vertices2[i + 1].get_x() > vertices2[i].get_x()) {
+					return false;
+				}
+			}
+		}
+		else if (diagonalAnalisada.getCoefAngular() > 0) {
+			if (vertices2[i].get_y() < vertices2[i + 2].get_y()) {
+				if (projecao < vertices2[i + 1].get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (projecao > vertices2[i + 1].get_y()) {
+					return false;
+				}
+			}
+
+		}
+		else if (diagonalAnalisada.getCoefAngular() < 0) {
+			if (vertices2[i].get_y() < vertices2[i + 2].get_y()) {
+				if (projecao > vertices2[i + 1].get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (projecao < vertices2[i + 1].get_y()) {
+					return false;
+				}
+			}
+		}
+		else {
+			if (vertices2[i].get_x() > vertices2[i + 2].get_x()) {
+				if (vertices2[i + 1].get_y() < vertices2[i].get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (vertices2[i + 1].get_y() > vertices2[i].get_y()) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+bool Poligono::contemPonto(Point ponto)
+{
+	vector<Point> vertices2 = vertices;
+	vertices2.push_back(vertices[0]);
+
+	for (int i = 0; i < arestas.size(); i++) {
+		cout << i;
+		double projecao = ponto.get_x() * arestas[i].getCoefAngular() + arestas[i].getCoefLinear();
+
+		if (arestas[i].getCoefAngular() == DBL_MAX) {
+			if (vertices2[i].get_y() < vertices2[i + 1].get_y()) {
+				if (ponto.get_x() > vertices2[i].get_x()) {
+					return false;
+				}
+			}
+			else {
+				if (ponto.get_x() < vertices2[i].get_x()) {
+					return false;
+				}
+			}
+		}
+
+		else if (arestas[i].getCoefAngular() > 0) {
+			if (vertices2[i].get_y() < vertices2[i + 1].get_y()) {
+				if (projecao < ponto.get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (projecao > ponto.get_y()) {
+					return false;
+				}
+			}
+		}
+
+		else if (arestas[i].getCoefAngular() < 0) {
+			if (vertices2[i].get_y() < vertices2[i + 1].get_y()) {
+				if (projecao < ponto.get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (projecao > ponto.get_y()) {
+					return false;
+				}
+			}
+		}
+		
+		else {
+			if (vertices2[i].get_x() < vertices2[i + 1].get_x()) {
+				if (ponto.get_y() < vertices2[i].get_y()) {
+					return false;
+				}
+			}
+			else {
+				if (ponto.get_y() > vertices2[i].get_y()) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+void Poligono::calcularArea()
+{
+	double areaPoligono = 0;
+
+	for (int i = 0; i != vertices.size()-1; i++) {
+		areaPoligono += vertices[i].get_x() * vertices[i + 1].get_y() - vertices[i + 1].get_x() * vertices[i].get_y();
+	}
+	areaPoligono += vertices[vertices.size() - 1].get_x() * vertices[0].get_y() - vertices[0].get_x() * vertices[vertices.size() - 1].get_y();
+	areaPoligono /= 2;
+
+	this->area = areaPoligono;
+}
+
+double Poligono::getArea()
+{
+	return this->area;
+}
